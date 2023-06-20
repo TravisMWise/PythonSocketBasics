8bf4734681d353afe163e1a7e590346e865182a7
Travis Wise <86330962+TravisMWise@users.noreply.github.com>
2023-06-20 14:20:54 -0400

    Send and recieve any python object using sockets

--- client.py
+++ client.py
@@ -1,22 +1,33 @@
 import socket
+import pickle
 
-HEADERSIZE = 20
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 4001))
+s.connect((socket.gethostname(), 4002))
 
 while True:
-    fullMsg = ''
+    fullMsg = b''
     newMsg = True
     while True:
-        msg = s.recv(20)
+        msg = s.recv(16)
+        print(msg)
         if newMsg:
             msgLen = int(msg[:HEADERSIZE])
             print(f"New message length: {msgLen}")
             newMsg = False
             
-        fullMsg += msg.decode("utf-8")
+        fullMsg += msg
+        
         if len(fullMsg) - HEADERSIZE == msgLen:
             print("Full message recieved:", fullMsg[HEADERSIZE:])
+            
+            # When we have all the data for the stucture we are
+            # done using the socket and can use the data as normal
+            d = pickle.loads(fullMsg[HEADERSIZE:])
+            print(d) # {1: 'Name', 2: 'Age'}
+            print(d[1]) # Name
+            print(d[2]) # Age
+
             newMsg = True
-            fullMsg = ''
+            fullMsg = b''
--- server.py
+++ server.py
@@ -1,24 +1,28 @@
 import socket
 import time
+import pickle
 
-HEADERSIZE = 20
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 4001))
+s.bind((socket.gethostname(), 4002))
 s.listen(5)
 
-def addHeader(str):
-    return f'{len(str):<{HEADERSIZE}}'+str
 while True:
+    # Wait for a connection
     clientSocket, address = s.accept()
     print(f"Connection from {address} established.")
 
-    msg = 'Welcome to the server!'
-    msg = addHeader(msg)
-    clientSocket.send(bytes(msg, "utf-8"))
+    # Get a data structure
+    d = {1:'Name', 2:'Age'}
+    
+    # Package the data up for sending using pickle 
+    msg = pickle.dumps(d)
+    
+    # Add a header with the length of the msg to send
+    # Convert the header to bytes to match the msg and socket format
+    msg = bytes(f'{len(msg):<{HEADERSIZE}}', 'utf-8') + msg
 
-    while True:
-        time.sleep(3)
-        timeMsg = f'The time is {time.time()}'
-        headedTimeMsg = addHeader(timeMsg) 
-        clientSocket.send(bytes(headedTimeMsg, 'utf-8'))
+    # Send the message
+    clientSocket.send(msg)
